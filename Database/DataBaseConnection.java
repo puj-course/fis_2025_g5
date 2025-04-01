fb8c9ea06efc92a8d75e313376918e916fb97651
juanguti1 <gu.juan@javeriana.edu.co>
2025-03-31 23:35:45 -0500

    #102 y #90 conexion de la BD con las historias de usuario rspectivas de registro usuario y login

---
+++ Database/DataBaseConnection.java
@@ -0,0 +1,24 @@
+package grupodefault.proyectofundamentos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataBaseConnection {
+public Connection databaseLink;
+public Connection getConnection() {
+    String nombrebasedatos = "sys";
+    String usuariobasedatos ="root";
+    String contrabasedatos = "Javeriana2024$";
+
+    String url = "jdbc:mysql://localhost/" + nombrebasedatos;
+
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        databaseLink = DriverManager.getConnection(url, usuariobasedatos, contrabasedatos);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+    return databaseLink;
+}
+    }
+
