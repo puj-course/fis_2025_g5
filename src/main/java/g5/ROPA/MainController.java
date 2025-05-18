7963ee09fe8173efda895346b11b5987541c50f1
Santiago Galindo <galindosantiago@javeriana.edu.co>
2025-05-18 17:19:57 -0500

    ✨ #115 Controller del Main

--- src/main/java/g5/ROPA/MainController.java
+++ src/main/java/g5/ROPA/MainController.java
@@ -1,17 +1,23 @@
 package g5.ROPA;
 
 import java.io.IOException;
+import java.util.*;
 
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class MainController {
+	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
@@ -19,6 +25,11 @@ public class MainController {
 	@FXML
 	private VBox field;
 	private Parent fxml;
+	
+	@FXML
+	private Button editarButton;
+	@FXML
+	private Label displayUsuaria;
 	
 	public void goToSignIn(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("SignPage.fxml"));
@@ -28,17 +39,18 @@ public class MainController {
 		stage.show();
 	}
 	
-	public void initialize () {
+	public void initialize () throws SQLException {
 		try {
 			fxml = FXMLLoader.load(getClass().getResource("Telas.fxml"));
 			field.getChildren().removeAll();
 			field.getChildren().setAll(fxml);
+			hayUsuario();
 		} catch (IOException ex) {
 	//		Logger.getLogger(MainController.class.getName()).log(Level.SEVERE,null,ex);
-		}	
+		}
 	}
 	
-	public void goToTelas(ActionEvent event) throws IOException {
+	public void goToTelas(ActionEvent event) throws IOException, SQLException {
 		try {
 			fxml = FXMLLoader.load(getClass().getResource("Telas.fxml"));
 			field.getChildren().removeAll();
@@ -66,5 +78,26 @@ public class MainController {
 		} catch (IOException ex) {
 	//		Logger.getLogger(MainController.class.getName()).log(Level.SEVERE,null,ex);
 		}	
+	}
+	
+	public void hayUsuario() {
+		User usuario = User.getInstance();
+		if(usuario.getNombre() == null) {
+			editarButton.setVisible(false);
+		} 
+		if((usuario.getTipo().equals("P") || usuario.getTipo().equals("E"))) {
+			editarButton.setVisible(true);
+			displayUsuaria.setText(usuario.getNombre());
+		} else {
+			displayUsuaria.setText(usuario.getNombre());
+		}
+	}
+	
+	public void goToEdit(ActionEvent event) throws IOException{
+		root = FXMLLoader.load(getClass().getResource("Editar.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 }
